1f501a547004fc55aa31812996c99764b14c030e
abuse.ch <41051806+abusech@users.noreply.github.com>
2019-04-15 12:49:38 +0200

    Added sanity checks

--- clamav.sh
+++ clamav.sh
@@ -14,32 +14,37 @@ CLAMGROUP="clamav"
 
 RELOAD=0
 
-lockfile -r 0 /tmp/local.the.lock || exit 1
+lockfile -r 0 /tmp/local.the.lock 2>/dev/null || exit 1
 
-rm -f /tmp/urlhaus.ndb
+rm -rf /tmp/urlhaus
+mkdir /tmp/urlhaus
 
-curl -s https://urlhaus.abuse.ch/downloads/urlhaus.ndb -o /tmp/urlhaus.ndb
+curl -s https://urlhaus.abuse.ch/downloads/urlhaus.ndb -o /tmp/urlhaus/urlhaus.ndb
 
 if [ $? -eq 0 ]; then
-  if [ -f "$CLAMDIR"/urlhaus.ndb ]; then
-    MD5old=`md5sum "$CLAMDIR"/urlhaus.ndb`
-    MD5new=`md5sum /tmp/urlhaus.ndb`
-    if ! [ "$MD5old" = "$MD5new" ]; then
-      # Updated file
-      cp /tmp/urlhaus.ndb $CLAMDIR
-      chown $CLAMUSER.$CLAMGROUP "$CLAMDIR"/urlhaus.ndb
-      RELOAD=1
-    fi
+  clamscan --quiet -d /tmp/urlhaus /tmp/urlhaus 2>&1 >/dev/null
+  if [ $? -eq 0 ]; then
+    if [ -f "$CLAMDIR"/urlhaus.ndb ]; then
+      MD5old=`md5sum "$CLAMDIR"/urlhaus.ndb`
+      MD5new=`md5sum /tmp/urlhaus/urlhaus.ndb`
+      if ! [ "$MD5old" = "$MD5new" ]; then
+        # Updated file
+        cp /tmp/urlhaus/urlhaus.ndb $CLAMDIR
+        chown $CLAMUSER.$CLAMGROUP "$CLAMDIR"/urlhaus.ndb
+        RELOAD=1
+      fi
     else
       # Looks like it's the first run
-      cp /tmp/urlhaus.ndb $CLAMDIR
+      cp /tmp/urlhaus/urlhaus.ndb $CLAMDIR
       chown $CLAMUSER.$CLAMGROUP "$CLAMDIR"/urlhaus.ndb
       RELOAD=1
     fi
+  fi
 fi
 
 if [ $RELOAD -eq 1 ]; then
   clamdscan --reload
 fi
 
+rm -rf /tmp/urlhaus
 rm -f /tmp/local.the.lock
