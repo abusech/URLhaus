a6571b43e48ab9735b51ada32a1095e98fdb6aae
Kili <mkilijanek@users.noreply.github.com>
2019-11-26 22:40:42 +0100

    Locking rebuild
    Rebuild locking mechanics for compatibility with newer distributions. Existing mechanic had issues on e.g. Ubuntu 18.04 LTS. Changes provided with this commit were tested and no issues were observed.

--- clamav.sh
+++ clamav.sh
@@ -6,6 +6,30 @@
 #
 # Please set up the following variables to fit your system
 
+### HEADER ###
+ 
+LOCKFILE="/var/lock/`basename $0`"
+LOCKFD=99
+
+# PRIVATE
+_lock()             { flock -$1 $LOCKFD; }
+_no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
+_prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
+
+# ON START
+_prepare_locking
+
+# PUBLIC
+exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
+exlock()            { _lock x; }   # obtain an exclusive lock
+shlock()            { _lock s; }   # obtain a shared lock
+unlock()            { _lock u; }   # drop a lock
+
+# Simplest example is avoiding running multiple instances of script.
+exlock_now || exit 1
+
+### BEGIN OF SCRIPT ###
+
 CLAMDIR="/var/lib/clamav"
 CLAMUSER="clamav"
 CLAMGROUP="clamav"
@@ -13,8 +37,6 @@ CLAMGROUP="clamav"
 # Don't change anything below this line
 
 RELOAD=0
-
-lockfile -r 0 /tmp/local.the.lock 2>/dev/null || exit 1
 
 rm -rf /tmp/urlhaus
 mkdir /tmp/urlhaus
@@ -47,4 +69,3 @@ if [ $RELOAD -eq 1 ]; then
 fi
 
 rm -rf /tmp/urlhaus
-rm -f /tmp/local.the.lock
