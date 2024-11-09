fd404785bf4cd82ce6a50525394f3bac9b06650a
abuse.ch <41051806+abusech@users.noreply.github.com>
2024-11-09 16:12:27 +0100

    Update lookup_url_bulk.py

--- lookup_url_bulk.py
+++ lookup_url_bulk.py
@@ -7,10 +7,16 @@ import os
 # Prepare HTTPSConnectionPool
 pool = urllib3.HTTPSConnectionPool('urlhaus-api.abuse.ch', port=443, maxsize=10)
 
-def query_urlhaus(url):
+def query_urlhaus(auth_key, url):
     # Construct the HTTP request
-    data = {'url' : url}
-    response = pool.request_encode_body("POST", "/v1/url/", fields=data, encode_multipart=False)
+    data = {
+        'url' : url
+    }
+    # Set the Authentication header
+    headers = {
+        "Auth-Key"      :   auth_key
+    }
+    response = pool.request_encode_body("POST", "/v1/url/", fields=data, encode_multipart=False, headers=headers)
     # Parse the response from the API
     response = response.data.decode("utf-8", "ignore")
     # Convert response to JSON
@@ -20,17 +26,18 @@ def query_urlhaus(url):
     elif json_response['query_status'] == 'no_results':
         print(f"[-] Not found: {url}")
     else:
-        print(f"[-] Error:     {url}")
+        print(f"[-] Error:     {url}: {json_response['query_status']}")
 
-if len(sys.argv) > 1:
-    if not os.path.isfile(sys.argv[1]):
+if len(sys.argv) > 2:
+    if not os.path.isfile(sys.argv[2]):
         print("Input file not found")
         quit()
-    file = open(sys.argv[1], 'r')
+    file = open(sys.argv[2], 'r')
     urls = file.readlines()
     for url in urls:
-        query_urlhaus(url.strip())
+        query_urlhaus(sys.argv[1], url.strip())
 else:
     print("Takes a local file name as argument and looks up each URL sequentialy on the URLhaus bulk API")
     print("Input file must contain one URL per line")
     print("Usage: python3 lookup_url_bulk.py <input file>")
+    print("Note: If you don't have an Auth-Key yet, you can obtain one at https://auth.abuse.ch/")
